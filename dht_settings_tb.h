3fad309326372c97d8675f6fc5113e41c43eab59
stageore <maxremonnay25@gmail.com>
2024-08-22 10:01:22 +0200

    Create dht_settings_tb.h
    Fichier contenant les données pour se connecter à ThingsBoard

---
+++ dht_settings_tb.h
@@ -0,0 +1,10 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+constexpr char WIFI_SSID[] = "Galaxy_S21";
+constexpr char WIFI_PASSWORD[] = "12345678";
+constexpr char TOKEN[] = "opCRlLoD55hAn0W5m5yb";
+constexpr char THINGSBOARD_SERVER[] = "thingsboard.cloud";
+constexpr int THINGSBOARD_PORT = 1883;
+
+#endif
