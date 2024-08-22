3d17e750fa8f694d2f3e8a53fbf2dda1c5d17b14
stageore <maxremonnay25@gmail.com>
2024-08-22 09:43:50 +0200

    Create settings_led_tb.h
    Fichier contenant les valeurs permettant la connexion entre la carte ESP32 et ThingsBoard.

---
+++ settings_led_tb.h
@@ -0,0 +1,8 @@
+constexpr char WIFI_SSID[] = "Galaxy_S21";
+constexpr char WIFI_PASSWORD[] = "12345678";
+constexpr char TOKEN[] = "opCRlLoD55hAn0W5m5yb";
+constexpr char THINGSBOARD_SERVER[] = "thingsboard.cloud";
+constexpr uint16_t THINGSBOARD_PORT = 1883U;
+constexpr uint32_t MAX_MESSAGE_SIZE = 1024U;
+constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
+constexpr size_t MAX_ATTRIBUTES = 2U;
